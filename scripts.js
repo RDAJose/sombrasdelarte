74372adec7da8c63c038a7c1f150e9f66361b2be
sombrasdelarte <josele.olmedobarrionuevo@gmail.com>
2025-12-18 20:12:22 +0100

    Cambio general prueba 2

--- scripts.js
+++ scripts.js
@@ -1,16 +1,20 @@
-// Script del formulario
+// CS07 - formulario
 const form = document.getElementById("form-consulta");
 const msg = document.getElementById("msg-form");
 
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-  msg.textContent = "✅ Consulta enviada correctamente.";
-  form.reset();
-});
+if (form) {
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    msg.textContent = "✅ Consulta enviada correctamente.";
+    form.reset();
+  });
+}
 
-// Cambio de estilos
-const btnTema = document.getElementById("btn-tema");
+// CS08 - cambio de estilo (GLOBAL)
+const btnTema = document.querySelector(".btn-tema");
 
-btnTema.addEventListener("click", function () {
-  document.body.classList.toggle("tema-claro");
-});
+if (btnTema) {
+  btnTema.addEventListener("click", function () {
+    document.body.classList.toggle("tema-claro");
+  });
+}
