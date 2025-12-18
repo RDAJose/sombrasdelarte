244aeed1d9e30f9992f9ef7bd6cf7f4be3ae07a1
sombrasdelarte <josele.olmedobarrionuevo@gmail.com>
2025-12-18 19:26:20 +0100

    Formulario coninteractividad

---
+++ scripts.js
@@ -0,0 +1,16 @@
+// CS07 - Script del formulario
+const form = document.getElementById("form-consulta");
+const msg = document.getElementById("msg-form");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  msg.textContent = "✅ Consulta enviada correctamente.";
+  form.reset();
+});
+
+// CS08 - Cambio de estilos
+const btnTema = document.getElementById("btn-tema");
+
+btnTema.addEventListener("click", function () {
+  document.body.classList.toggle("tema-claro");
+});
